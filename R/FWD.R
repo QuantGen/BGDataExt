7c69aafab49c079d62f0e8632f04fa0744ae52b8
Alexander Grueneberg <git@agrueneberg.info>
2021-06-09 10:27:29 -0400

    Use active membership vector instead of b != 0
    The intercept should be part of the active predictors, but may or may
    not be depending on whether y has been centered or not.

--- R/FWD.R
+++ R/FWD.R
@@ -15,6 +15,8 @@ FWD <- function(y, X, df = 20, tol = 1e-7, maxIter = 1000, centerImpute = TRUE, 
     rhs <- crossprod(X, y)
     n <- length(y)
     p <- ncol(X)
+    active <- rep(FALSE, p)
+    names(active) <- colNames
     B <- matrix(data = 0, nrow = p, ncol = df)
     rownames(B) <- colNames
     RSS <- rep(NA_real_, df)
@@ -24,6 +26,7 @@ FWD <- function(y, X, df = 20, tol = 1e-7, maxIter = 1000, centerImpute = TRUE, 
     AIC <- rep(NA_real_, df)
     BIC <- rep(NA_real_, df)
     path <- rep(NA_character_, df)
+    active[1] <- TRUE
     B[1, 1] <- mean(y)
     RSS[1] <- sum((y - B[1, 1])^2)
     DF[1] <- 1
@@ -34,15 +37,24 @@ FWD <- function(y, X, df = 20, tol = 1e-7, maxIter = 1000, centerImpute = TRUE, 
     path[1] <- colNames[1]
     tol <- tol * RSS[1]
     for (i in 2:df) {
-        tmp <- addOne(C = C, rhs = rhs, b = B[, i - 1], RSS = RSS[i - 1], maxIter = maxIter, tol = tol)
+        tmp <- addOne(
+            C = C,
+            rhs = rhs,
+            active = active,
+            b = B[, i - 1],
+            RSS = RSS[i - 1],
+            maxIter = maxIter,
+            tol = tol
+        )
         B[, i] <- tmp[["b"]]
         if (length(tmp[["newPred"]]) > 0) {
+            active[tmp[["newPred"]]] <- TRUE
             path[i] <- colNames[tmp[["newPred"]]]
         } else {
             path[i] <- NA
         }
         RSS[i] <- tmp[["RSS"]]
-        DF[i] <- sum(tmp[["b"]] != 0)
+        DF[i] <- sum(active)
         VARE[i] <- RSS[i] / (n - DF[i])
         LogLik[i] <- -(n / 2) * log(2 * pi * VARE[i]) - RSS[i] / VARE[i] / 2
         AIC[i] <- -2 * LogLik[i] + 2 * (DF[i] + 1)
@@ -66,29 +78,45 @@ FWD <- function(y, X, df = 20, tol = 1e-7, maxIter = 1000, centerImpute = TRUE, 
     return(OUT)
 }
 
-addOne <- function(C, rhs, b, RSS, maxIter, tol) {
-    inactive <- which(abs(b) < sqrt(.Machine$double.eps))
-    active <- seq_along(b)[-inactive]
-    nInactive <- length(inactive)
-    nActive <- length(active)
+addOne <- function(C, rhs, active, b, RSS, maxIter, tol) {
+    activeSet <- which(active)
+    inactiveSet <- which(!active)
+    nActive <- length(activeSet)
+    nInactive <- length(inactiveSet)
+    # if model is not null
+    if (nActive > 1) {
+        RSSNew <- rep(NA_real_, nInactive)
+        for (i in 1:nInactive) {
+            fm <- fitSYS(
+                C = C,
+                rhs = rhs,
+                b = b,
+                active = c(inactiveSet[i], activeSet),
+                RSS = RSS,
+                maxIter = maxIter,
+                tol = tol
+            )
+            RSSNew[i] <- fm[["RSS"]]
+        }
+        k <- which.min(RSSNew)
+        fm <- fitSYS(
+            C = C,
+            rhs = rhs,
+            b = b,
+            active = c(inactiveSet[k], activeSet),
+            RSS = RSS,
+            maxIter = maxIter,
+            tol = tol
+        )
+        ans <- list(b = fm[["b"]], newPred = inactiveSet[k], RSS = fm[["RSS"]])
     # if model is null
-    if (nActive == 0) {
+    } else {
         bOLS <- rhs / diag(C)
         dRSS <- diag(C) * bOLS^2
         k <- which.max(dRSS)
         b[k] <- bOLS[k]
         RSS <- RSS - bOLS[k]^2 * C[k, k]
         ans <- list(b = b, newPred = k, RSS = RSS)
-    # when model is not null
-    } else {
-        RSSNew <- rep(NA_real_, nInactive)
-        for (i in 1:nInactive) {
-            fm <- fitSYS(C = C, rhs = rhs, b = b, active = c(inactive[i], active), RSS = RSS, maxIter = maxIter, tol = tol)
-            RSSNew[i] <- fm[["RSS"]]
-        }
-        k <- which.min(RSSNew)
-        fm <- fitSYS(C = C, rhs = rhs, b = b, active = c(inactive[k], active), RSS = RSS, maxIter = maxIter, tol = tol)
-        ans <- list(b = fm[["b"]], newPred = inactive[k], RSS = fm[["RSS"]])
     }
     return(ans)
 }
