e15bbdc5e26b82b128179e277e957eb871004b32
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-06-30 08:41:11 -0400

    Update fitLSYS.R

--- R/fitLSYS.R
+++ R/fitLSYS.R
@@ -1,14 +1,1 @@
-
-fitLSYS=function(C,rhs,p=nrow(C),b=rep(0,p),active=1:p,RSS=1e5,maxIter=10,tol=1e-4){          
-  
-    fm <- fitSYS(
-                C = C,
-                rhs = rhs,
-                b = b,
-                active = c(inactiveSet[i], activeSet),
-                RSS = RSS,
-                maxIter = maxIter,
-                tol = tol
-            )
-      return(fm)
-}
+GS=fitSYS
