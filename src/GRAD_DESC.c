a69b987f490b5b3d38ddffcd8400943cc77d7339
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-09-20 10:21:21 -0400

    Update GRAD_DESC.c

--- src/GRAD_DESC.c
+++ src/GRAD_DESC.c
@@ -11,23 +11,7 @@ SEXP GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP active, SEXP nIter, SEXP learning_
     double *pb = REAL(b);
     int *pactive = INTEGER(active);
 
-    for (int iter = 0; iter < nIter; iter++) {
-        
-        for (int j = 0; j < nActive; j++) { // loop over active predictors
-            int k = pactive[j];
-            double Ckk = pC[k * (p + 1)];
-            double offset = 0;      
-            
-            for (int m = 0; m < nActive; m++) {
-                int n = pactive[m];
-                offset += pC[p * k + n] * pb[n];
-            }
-            
-            double gradient=prhs[k]-offset
-            double sol=pb[k]-learning_rate*gradient
-            pb[k] = sol;
-        }
-    }
+
     // Creating a list to return results
     SEXP list = PROTECT(Rf_allocVector(VECSXP, 2));
     SET_VECTOR_ELT(list, 0, b);
