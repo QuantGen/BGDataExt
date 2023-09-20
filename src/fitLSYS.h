9e18474309d92599759583d0581d68a773264f0c
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-09-20 10:15:43 -0400

    Update fitLSYS.h

--- src/fitLSYS.h
+++ src/fitLSYS.h
@@ -3,5 +3,3 @@
 #include <Rinternals.h>
 
 SEXP fitLSYS(SEXP C, SEXP rhs, SEXP b, SEXP active, SEXP RSS, SEXP maxIter, SEXP tolerance);
-
-SEXP fitLSYS(SEXP C, SEXP rhs, SEXP b, SEXP active, SSEXP nIter, SEXP learning_rate);
