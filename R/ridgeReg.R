ff2c9a6c54725bc673ff579515e2679a7ad48c96
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-09-28 15:07:33 -0400

    Update ridgeReg.R

--- R/ridgeReg.R
+++ R/ridgeReg.R
@@ -1,8 +1,15 @@
 # A wrappper for ridge regression, possibly shrinking towards b0
-RR <- function(XX, Xy,lambda,p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda2=0, active=1:p, RSS=1, maxIter=1000, tol=1e-5) {
-    active <- active - 1L # for the 0-based index
+ RR <- function(XX, Xy,lambda,p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda2=0, active=1:p, RSS=10000, maxIter=1000, tol=1e-5) {
+     
+    # adding the shrinkage parameter to the left-hand-side
     diag(XX)=diag(XX)+lambda
-    Xy=Xy+lambda*b0
-    ans <- .Call("fitLSYS", XX, Xy, b, active, RSS, maxIter, tol)
-    return(list(b = ans[[1]], RSS = ans[[2]]))
-}
+
+     # adding prior mean to the right-hand-side
+    if(lambda2>0){
+      Xy=Xy+lambda2*b0
+    }
+    
+    ans<-fitSYS(XX, Xy, b, active, RSS, maxIter, tol)[[1]] 
+   
+    return(ans)
+ }
