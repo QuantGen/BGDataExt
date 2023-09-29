fd7f10e50357426c342aba3246322594155dd3a6
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-09-29 08:57:17 -0400

    Update GRAD_DESC.R

--- R/GRAD_DESC.R
+++ R/GRAD_DESC.R
@@ -1,8 +1,9 @@
 # A wrapper to the C-function that performs Gradient Descent in a system of linear equations
 
-GD<- function(XX,Xy,b=rep(0,ncol(XX)),active=1:ncol(XX), RSS=1,nIter=10,learning_rate=1/mean(diag(XX))/10,lambda=0,b0=rep(0,ncol(XX)),lambda0=1){
-    #C, rhs, b, active=1:ncol(C), RSS=1, nIter, learning_rate) {
+GD<- function(XX,Xy,b=rep(0,ncol(XX)),active=1:ncol(XX), RSS=1,nIter=10,learning_rate=1/5,lambda=0,b0=rep(0,ncol(XX)),lambda0=1){
+    
     diag(XX)=diag(XX)+lambda
+    learning_rate=learning_rate/mean(diag(XX))
     Xy=Xy+lambda*lambda0*b0
     active <- active - 1L # for the 0-based index
         ans <- .Call("GRAD_DESC",XX, Xy, b, active, nIter, learning_rate)
